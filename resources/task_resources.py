7e75d1743f7992cbef27dc88ed123d5c26341932
Hieu Tran <hanhieu.a3.namly.2012@gmail.com>
2017-03-09 21:16:24 +0700

    add edit , delete api

--- resources/task_resources.py
+++ resources/task_resources.py
@@ -26,3 +26,26 @@ class TaskRes(Resource):
     def get(self,task_id):
         task = Task.objects().with_id(task_id)
         return mlab.item2json(task)
+    def put(self,task_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument(name="name", type=str, location="json")
+        parser.add_argument(name="local_id", type=str, location="json")
+        parser.add_argument(name="done", type=bool, location="json")
+
+        body = parser.parse_args()
+        name = body["name"]
+        local_id = body["local_id"]
+        done = body["done"]
+
+        task = Task.objects().with_id(task_id)
+        task.update(name=name, local_id=local_id, done=done)
+        task_update = Task.objects().with_id(task_id)
+        return mlab.item2json(task_update)
+    def delete(self,task_id):
+        def delete(self, task_id):
+            task = Task.objects().with_id(task_id)
+            if task == None:
+                return {"message": "Task not found"}
+            else:
+                task.delete()
+                return {"message": "Delete Success"}
